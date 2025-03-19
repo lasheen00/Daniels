23d87a4ee902cffbcb978305366b117584183f9b
lasheen00 <las7een00@gmail.com>
2025-03-19 05:16:58 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//!-----Typed JS-----//!
+var typed = new Typed('#element', {
+    strings: ["Larry Daniels","Developer","Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop:true
+  });
+//!-----Typed JS-----//!
+
+
+
+
+
+
+//!----- -----//!
+//!----- -----//!
+
+
+
+
